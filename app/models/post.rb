d108c204a9f8cc45c4035cdcad686bf3fc4e7e1f
ahmedadel56 <ahmdadl563@gmail.com>
2022-07-08 18:11:34 +0200

    Setup rspec gem And Create test folders

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,7 @@ class Post < ApplicationRecord
   has_many :likes
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comment_counter, :likes_counter, numericality: {only_integer: true, greater_than_or_equal_to: 0 }
-
+  validates :comment_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     user.increment!(:posts_counter)
