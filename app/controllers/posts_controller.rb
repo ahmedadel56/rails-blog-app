7ecbdd2af3dca4fc426aa31cce0b5821df927e09
ahmedadel56 <ahmdadl563@gmail.com>
2022-07-15 13:45:16 +0200

    Allow Users to add likes to Posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     @post = Post.find_by(user_id: @user.id, id: params[:id])
     @comments = @post.recent_comments
   end
-  
+
   def new
     @post = Post.new
   end
@@ -18,13 +18,15 @@ class PostsController < ApplicationController
     @user = current_user
     @new_post = current_user.posts.new(post_params)
     @new_post.update_posts_counter
-    if @new_post.save 
+    if @new_post.save
       redirect_to user_posts_path, notice: 'succeded'
     else
       render :new
     end
   end
+
   private
+
   def post_params
     params.require(:data).permit(:title, :text)
   end
