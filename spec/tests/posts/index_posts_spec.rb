e9e1dc2270e7e938cbf528f7abbc1a23f47bd9e5
ahmedadel56 <ahmdadl563@gmail.com>
2022-07-20 22:34:52 +0200

    Fix linter errors in posts index file

--- spec/tests/posts/index_posts_spec.rb
+++ spec/tests/posts/index_posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'User posts', type: :feature do
-  scenario 'user posts page', js: true do
+  before(:all) do
     @user_one = User.create(name: 'Ahmed1', photo: 'https://unsplash.com/photos/WS-1',
                             bio: 'I am not done yet this is user 1', posts_counter: 3)
     @user_two = User.create(name: 'Ahmed2', photo: 'https://unsplash.com/photos/WS-2',
@@ -16,17 +16,20 @@ RSpec.describe 'User posts', type: :feature do
     @post_three = Post.create(user_id: @user_one.id, title: 'Hello post 3', text: 'This is my 3 post', likes_counter: 3,
                               comment_counter: 3)
 
-    @comment_1_post_one = Comment.create(post_id: @post_one.id, user_id: @user_two.id, text: 'Hi Ahmed this is Ahmed 2')
+    @comment_1_post_one = Comment.create(post_id: @post_one.id, user_id: @user_two.id,
+                                         text: 'Hi Ahmed this is Ahmed 2')
     @comment_2_post_one = Comment.create(post_id: @post_one.id, user_id: @user_three.id,
                                          text: 'Hi Ahmed this is Ahmed 3')
-    @comment_1_post_two = Comment.create(post_id: @post_two.id, user_id: @user_two.id, text: 'Hi Ahmed this is Ahmed 2')
+    @comment_1_post_two = Comment.create(post_id: @post_two.id, user_id: @user_two.id,
+                                         text: 'Hi Ahmed this is Ahmed 2')
     @comment_2_post_two = Comment.create(post_id: @post_two.id, user_id: @user_three.id,
                                          text: 'Hi Ahmed this is Ahmed 3')
     @comment_1_post_three = Comment.create(post_id: @post_three.id, user_id: @user_two.id,
                                            text: 'Hi Ahmed this is Ahmed 2')
     @comment_2_post_three = Comment.create(post_id: @post_three.id, user_id: @user_three.id,
                                            text: 'Hi Ahmed this is Ahmed 3')
-
+  end
+  scenario 'user posts page', js: true do
     visit users_path
     click_link @user_one.name
     expect(current_path).to eql(user_path(@user_one.id))
@@ -52,9 +55,14 @@ RSpec.describe 'User posts', type: :feature do
     expect(page).to have_content(@post_one.comment_counter.to_s)
     expect(page).to have_content(@post_two.comment_counter.to_s)
     expect(page).to have_content(@post_three.comment_counter.to_s)
+  end
+  scenario 'senario 2 of user posts page', js: true do
+    visit users_path
+    click_link @user_one.name
+    expect(current_path).to eql(user_path(@user_one.id))
+    click_link 'See User Posts'
+    expect(current_path).to eql(user_posts_path(@user_one.id))
 
-    # redirect to post show
-    # post 1 test
     click_link @post_one.title
     expect(current_path).to eql(user_post_path(@user_one.id, @post_one.id))
     # post 2 test
