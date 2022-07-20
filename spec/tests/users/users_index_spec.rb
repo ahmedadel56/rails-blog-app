97073cb3c1f327212e62dac629ebe739e79eb59a
ahmedadel56 <ahmdadl563@gmail.com>
2022-07-20 14:36:22 +0200

    Finsih all user show page specs

--- spec/tests/users/users_index_spec.rb
+++ spec/tests/users/users_index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :feature do
   scenario 'users index page' do
-    user1 = User.create(name: 'medhat', photo: 'medhat.com', bio: 'I am done here')
-    user2 = User.create(name: 'Ahmed', photo: 'medhat.com', bio: 'I am not done yet')
+    user1 = User.create(name: 'medhat', photo: 'https://unsplash.com/photos/WS-zNNx9Pvc', bio: 'I am done here')
+    user2 = User.create(name: 'Ahmed', photo: 'https://unsplash.com/photos/WS-zNNx9Pvc', bio: 'I am not done yet')
     visit users_path
     expect(page).to have_content(user1.name)
     expect(page).to have_content(user2.name)
@@ -13,11 +13,8 @@ RSpec.describe 'Users', type: :feature do
     expect(page).to have_content(user2.posts_counter.to_s)
     click_link user1.name
     expect(current_path).to eql(user_path(user1.id))
-    sleep 1
     visit users_path
-    sleep 1
     click_link user2.name
-    sleep 1
     expect(current_path).to eql(user_path(user2.id))
   end
 end
--- spec/tests/users/users_show_spec.rb
+++ spec/tests/users/users_show_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'User', type: :feature do
   scenario 'user profile page', js: true do
-    user = User.create(name: 'Ahmed', photo: 'medhat.com', bio: 'I am not done yet')
+    user = User.create(name: 'Ahmed', photo: 'https://unsplash.com/photos/WS-zNNx9Pvc', bio: 'I am not done yet')
     count = 1
     5.times do |_post|
-      Post.create(user_id: user.id, title: "Hello #{count}", text: "This is my #{count} post")
+      Post.create(user_id: user.id, title: "Hello#{count}", text: "This is my #{count} post")
       count += 1
     end
     visit users_path
@@ -20,5 +20,13 @@ RSpec.describe 'User', type: :feature do
     expect(page).to have_content(user.posts[2].text)
     expect(page).to_not have_content(user.posts[3].text)
     expect(page).to_not have_content(user.posts[4].text)
+    # I can see a button that lets me view all of a user's posts.
+    click_link 'See User Posts'
+    expect(current_path).to eql(user_posts_path(user.id))
+    #  When I click a user's post, it redirects me to that post's show page.
+    visit users_path
+    click_link user.name
+    click_link user.posts[0].title
+    expect(current_path).to eql(user_post_path(user.id, user.posts[0].id))
   end
 end
