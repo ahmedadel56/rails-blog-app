9a572368f5ad6619a442ad279b899d4d90eb232b
ahmedadel56 <ahmdadl563@gmail.com>
2022-10-07 04:55:27 +0200

    Style login, sign up and Add image active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-  has_many :posts
-  has_many :comments, through: :posts
-  has_many :likes, through: :posts
+         :recoverable, :rememberable, :validatable
 
+  has_many :posts, :dependent => :destroy
+  has_many :comments, through: :posts, :dependent => :destroy
+  has_many :likes, through: :posts , :dependent => :destroy
+  has_one_attached :image, :dependent => :destroy
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
