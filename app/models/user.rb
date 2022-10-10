274b71ede49a9e328191b7125d6b4b52b5e12b20
ahmedadel56 <ahmdadl563@gmail.com>
2022-10-10 06:56:19 +0200

    fix linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts, :dependent => :destroy
-  has_many :comments, through: :posts, :dependent => :destroy
-  has_many :likes, through: :posts , :dependent => :destroy
-  has_one_attached :image, :dependent => :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, through: :posts, dependent: :destroy
+  has_many :likes, through: :posts, dependent: :destroy
+  has_one_attached :image, dependent: :destroy
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
